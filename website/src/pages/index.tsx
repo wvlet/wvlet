b838742e6d9a945a0a32e8a021ffa7a1c56e5293
Taro L. Saito <leo@xerial.org>
2024-09-08 21:46:37 -0700

    Fix link

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -24,7 +24,7 @@ function HomepageHeader() {
                                 <em className={'text--left'}>Wvlet is a cross-SQL flow-style query language for
                                     functional data modeling and interactive data analysis.</em>
                                 <div className={styles.buttons}>
-                                    <Link className="button button--primary" to="/docs/install">
+                                    <Link className="button button--primary" to="/docs/usage/install">
                                         Installation
                                     </Link>
                                     <Link className="button button--secondary margin--sm" to="/docs/">
