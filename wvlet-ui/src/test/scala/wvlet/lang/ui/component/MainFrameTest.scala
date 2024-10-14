7abf4129983b685e8b053db1008ea257665e9769
Taro L. Saito <leo@xerial.org>
2024-10-14 14:11:05 -0700

    Fix

--- wvlet-ui/src/test/scala/wvlet/lang/ui/component/MainFrameTest.scala
+++ wvlet-ui/src/test/scala/wvlet/lang/ui/component/MainFrameTest.scala
@@ -13,10 +13,10 @@
  */
 package wvlet.lang.ui.component
 
+import wvlet.airframe.rx.html.all.*
 import wvlet.airspec.AirSpec
-import wvlet.lang.ui.component.MainFrame
 
 class MainFrameTest extends AirSpec:
   test("sanity test") {
-    new MainFrame().render
+    MainFrame.render(div("hello wvlet"))
   }
