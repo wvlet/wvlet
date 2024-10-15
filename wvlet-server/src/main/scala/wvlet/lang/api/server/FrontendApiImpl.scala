8cca4999ccce17993c7d1820deefe2bab84809ff
Taro L. Saito <leo@xerial.org>
2024-10-15 16:10:08 -0700

    receive a query from UI

--- wvlet-server/src/main/scala/wvlet/lang/api/server/FrontendApiImpl.scala
+++ wvlet-server/src/main/scala/wvlet/lang/api/server/FrontendApiImpl.scala
@@ -5,12 +5,15 @@ import wvlet.airframe.ulid.ULID
 import wvlet.lang.api.v1.frontend.FrontendApi
 import wvlet.lang.api.v1.frontend.FrontendApi.*
 import wvlet.lang.api.v1.query.QueryInfo
+import wvlet.log.LogSupport
 
-class FrontendApiImpl() extends FrontendApi:
+class FrontendApiImpl() extends FrontendApi with LogSupport:
   private val startTimeNs = System.nanoTime()
 
   override def status: ServerStatus = ServerStatus(upTime = ElapsedTime.nanosSince(startTimeNs))
 
-  override def submitQuery(request: QueryRequest): QueryResponse = ???
+  override def submitQuery(request: QueryRequest): QueryResponse =
+    debug(request)
+    QueryResponse(queryId = ULID.newULID, requestId = request.requestId)
 
   override def getQueryInfo(request: QueryInfoRequest): QueryInfo = ???
--- wvlet-ui-main/src/main/scala/wvlet/lang/ui/editor/WvletMonacoEditor.scala
+++ wvlet-ui-main/src/main/scala/wvlet/lang/ui/editor/WvletMonacoEditor.scala
@@ -3,14 +3,16 @@ package wvlet.lang.ui.editor
 import org.scalablytyped.runtime.StringDictionary
 import wvlet.airframe.rx.html.RxElement
 import wvlet.airframe.rx.html.all.*
-import typings.monacoEditor.mod.editor
-import typings.monacoEditor.mod.languages
+import typings.monacoEditor.mod.{IKeyboardEvent, KeyCode, editor, languages}
 import org.scalajs.dom
-import typings.monacoEditor.mod.editor.BuiltinTheme
+import typings.monacoEditor.mod.editor.{BuiltinTheme, IStandaloneCodeEditor}
+import wvlet.lang.api.v1.frontend.FrontendApi
+import wvlet.lang.api.v1.frontend.FrontendApi.QueryRequest
+import wvlet.lang.api.v1.frontend.FrontendRPC.RPCAsyncClient
 
 import scala.scalajs.js
 
-class WvletMonacoEditor extends RxElement:
+class WvletMonacoEditor(rpcClient: RPCAsyncClient) extends RxElement:
 
   private def editorTheme: editor.IStandaloneThemeData =
     val theme = editor.IStandaloneThemeData(
@@ -30,6 +32,8 @@ class WvletMonacoEditor extends RxElement:
       )
     )
     theme
+
+  private var textEditor: IStandaloneCodeEditor = null
 
   private def monacoEditorOptions: editor.IStandaloneEditorConstructionOptions =
     val languageId = "wvlet"
@@ -76,10 +80,26 @@ class WvletMonacoEditor extends RxElement:
 
   end monacoEditorOptions
 
-  override def onMount: Unit = editor.create(
-    dom.document.getElementById("editor").asInstanceOf[dom.HTMLElement],
-    monacoEditorOptions
-  )
+  private def buildEditor: Unit =
+    textEditor = editor.create(
+      dom.document.getElementById("editor").asInstanceOf[dom.HTMLElement],
+      monacoEditorOptions
+    )
+    // Add shortcut keys
+    textEditor.onKeyDown { (e: IKeyboardEvent) =>
+      // ctrl + enter to submit the query
+      if e.keyCode == KeyCode.Enter && (e.ctrlKey || e.metaKey) then
+        rpcClient
+          .FrontendApi
+          .submitQuery(QueryRequest(query = getTextValue))
+          .run { case resp =>
+            info(s"Query result: ${resp}")
+          }
+    }
+
+  def getTextValue: String = textEditor.getValue()
+
+  override def onMount: Unit = buildEditor
 
   override def render = div(cls -> "pl-0 pr-2", div(id -> "editor", style -> "min-height: 250px;"))
 
