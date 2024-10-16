233f1e81e4fe3012037dd42cedab6e827a4665f9
Taro L. Saito <leo@xerial.org>
2024-10-16 16:40:54 -0700

    Add color to logs

--- wvlet-ui-main/src/main/scala/wvlet/lang/ui/editor/ConsoleLogWindow.scala
+++ wvlet-ui-main/src/main/scala/wvlet/lang/ui/editor/ConsoleLogWindow.scala
@@ -8,18 +8,20 @@ import wvlet.log.LogTimestampFormatter
 
 object ConsoleLog:
 
-  private def currentTimeStamp(): String = LogTimestampFormatter
-    .formatTimestamp(System.currentTimeMillis())
-
-  private var logBuffer: List[String] = List(
-    s"${currentTimeStamp()} wvlet version:${BuildInfo.version}"
+  private def currentTimeStamp(): RxElement = span(
+    cls -> "text-blue-300",
+    LogTimestampFormatter.formatTimestamp(System.currentTimeMillis())
   )
 
-  val logMessages: RxVar[List[String]] = Rx.variable(logBuffer)
-  private val bufferSize               = 1000
+  private var logBuffer: List[RxElement] = List(
+    span(currentTimeStamp(), " ", span(s"wvlet version:${BuildInfo.version}"))
+  )
+
+  val logMessages: RxVar[List[RxElement]] = Rx.variable(logBuffer)
+  private val bufferSize                  = 1000
 
   def write(log: String): Unit =
-    logBuffer = s"${currentTimeStamp()} ${log}" :: logBuffer.take(bufferSize - 1)
+    logBuffer = span(currentTimeStamp(), " ", span(log)) :: logBuffer.take(bufferSize - 1)
     logMessages := logBuffer.reverse
 
 class ConsoleLogWindow extends RxElement:
@@ -35,7 +37,7 @@ class ConsoleLogWindow extends RxElement:
         .logMessages
         .map { logBuffer =>
           logBuffer.map { log =>
-            code(log + "\n")
+            code(log, "\n")
           }
         }
     )
