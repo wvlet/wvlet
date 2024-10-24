6794ff504508d485ec53c6b90585690220a4adcb
Taro L. Saito <leo@xerial.org>
2024-10-23 22:56:39 -0700

    editor (fix): Properly iterate until query completion (#350)

--- wvlet-ui-main/src/main/scala/wvlet/lang/ui/editor/QueryResultReader.scala
+++ wvlet-ui-main/src/main/scala/wvlet/lang/ui/editor/QueryResultReader.scala
@@ -31,15 +31,15 @@ class QueryResultReader(rpcClient: RPCAsyncClient) extends LogSupport:
         lst += queryInfo
       )
 
-    val rx = getQueryInfo(1).transformRx {
-      case Success(queryInfo) =>
-        if queryInfo.status.isFinished then
-          Rx.single(queryInfo)
-        else
-          Rx.delay(300, TimeUnit.MILLISECONDS)
-            .flatMap(_ => getQueryInfo(queryInfo.pageToken.toInt + 1))
-      case Failure(e) =>
-        Rx.exception(e)
+    def loop(page: Int): Rx[QueryInfo] = getQueryInfo(page).flatMap { queryInfo =>
+      if queryInfo.status.isFinished then
+        Rx.single(queryInfo)
+      else
+        Rx.delay(300, TimeUnit.MILLISECONDS).flatMap(_ => loop(page + 1))
+    }
+
+    val rx = loop(1).tapOnFailure { e =>
+      ConsoleLog.writeError(s"Failed to fetch query result: ${e.getMessage}")
     }
     rx.run { _ =>
       val queryInfo = lst.result()
