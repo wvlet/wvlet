96855b995052b03ad69fe09ae0d4a1cf72dc3c42
Taro L. Saito <leo@xerial.org>
2024-10-26 22:36:53 -0700

    editor (fix #366): Use Consolas for fixed-width bars (#369)

--- wvlet-ui-main/tailwind.config.js
+++ wvlet-ui-main/tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 export default {
   content: [
     "./index.html",
     "./target/**/*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mono: ['Consolas', ...defaultTheme.fontFamily.mono],
+      },
+    },
   },
   plugins: [],
 }
