cc795d6aeb9c6e50c39dd17037e5b61d9d911582
Taro L. Saito <leo@xerial.org>
2024-11-15 10:53:22 -0800

    Check output

--- wvlet-native-cli/src/test/scala/wvlet/lang/native/WvcMainTest.scala
+++ wvlet-native-cli/src/test/scala/wvlet/lang/native/WvcMainTest.scala
@@ -3,14 +3,34 @@ package wvlet.lang.native
 import wvlet.airspec.AirSpec
 
 class WvcMainTest extends AirSpec:
+
+  def captureOut(body: => Unit): String =
+    val out = new java.io.ByteArrayOutputStream()
+    Console.withOut(out) {
+      body
+    }
+    debug(out)
+    out.toString
+
   test("run command") {
-    WvcMain.main(Array("-q", "select 1"))
+    val out = captureOut {
+      WvcMain.main(Array("-q", "select 1"))
+    }
+    out shouldContain "select 1"
   }
 
   test("use stdlib") {
-    WvcMain.main(Array("-q", "select '1'.to_int"))
+    val out = captureOut {
+      WvcMain.main(Array("-q", "select '1'.to_int"))
+    }
+    out shouldContain "cast('1' as bigint)"
   }
 
   test("load spec") {
-    WvcMain.main(Array("-w", "spec/basic", "-q", "from person"))
+    val out = captureOut {
+      WvcMain.main(Array("-w", "spec/basic", "-q", "from person"))
+    }
+    out shouldContain "from 'spec/basic/person.json'"
   }
+
+end WvcMainTest
