42390e15aeb19e27db7e49eb1e7f2e4209cab72a
Taro L. Saito <leo@xerial.org>
2024-11-22 15:49:17 -0800

    editor (feature: #473): Add more example queries

---
+++ wvlet-ui-playground/src/main/scala/wvlet/lang/ui/playground/QuerySelector.scala
@@ -0,0 +1,46 @@
+package wvlet.lang.ui.playground
+
+import wvlet.airframe.rx.html.RxElement
+import wvlet.airframe.rx.html.all.*
+
+import scala.collection.immutable.ListMap
+
+class QuerySelector(currentQuery: CurrentQuery, queryEditor: QueryEditor) extends RxElement:
+  override def render =
+    def separator(): RxElement = div(cls -> "border-t border-gray-600 mt-2 mb-2")
+    div(
+      cls -> "h-full bg-slate-700 p-3 text-sm text-slate-200",
+      h2(cls -> "text-slate-400", "Playground"),
+      separator(),
+      DemoQuerySet
+        .demoQueries
+        .map { demoQuerySet =>
+          div(
+            h2(cls -> "text-sm font-bold text-slate-300", demoQuerySet.name),
+            ul(
+              demoQuerySet
+                .queries
+                .map { q =>
+                  li(
+                    cls -> "text-slate-400 hover:text-white px-2",
+                    a(
+                      href -> "#",
+                      q.name,
+                      onclick -> { e =>
+                        e.preventDefault()
+                        currentQuery.queryName  := q.name
+                        currentQuery.wvletQuery := q.query
+                        queryEditor.setText(q.query)
+                      }
+                    )
+                  )
+                }
+            )
+          )
+        },
+      separator()
+    )
+
+  end render
+
+end QuerySelector
