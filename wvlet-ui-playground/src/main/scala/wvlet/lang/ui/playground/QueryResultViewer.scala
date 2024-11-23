d06f0d2f82ffbf2c1bd4a05afc6b82120d2b7ea5
Taro L. Saito <leo@xerial.org>
2024-11-22 18:07:56 -0800

    editor (feature: #473): Fix preview window height

--- wvlet-ui-playground/src/main/scala/wvlet/lang/ui/playground/QueryResultViewer.scala
+++ wvlet-ui-playground/src/main/scala/wvlet/lang/ui/playground/QueryResultViewer.scala
@@ -6,20 +6,22 @@ import wvlet.lang.api.v1.query.QueryResult
 import wvlet.lang.compiler.TablePrinter
 
 class QueryResultViewer(currentQuery: CurrentQuery) extends RxElement:
-  override def render: RxElement = div(
-    cls   -> "bg-zinc-800 text-xs text-slate-300 dark:text-white p-2",
-    style -> s"height: ${PlaygroundUI.previewWindowHeightPx}px;",
-    div(cls -> "h-7 font-light text-slate-400", "Preview"),
+  override def render: RxElement =
+    val consoleHeight = PlaygroundUI.previewWindowHeightPx - 50
     div(
-      // Important: Setting the width relative to the viewport width in order to hide overflowed contents
-      style -> "overflow-x: scroll; overflow-y: auto; max-width: calc(100vw - 184px);",
-      currentQuery
-        .lastQueryResult
-        .map { result =>
-          pre(cls -> "font-mono", printQueryResult(result))
-        }
+      cls -> "bg-zinc-800 text-xs text-slate-300 dark:text-white p-2",
+      div(cls -> "h-7 font-light text-slate-400", "Preview"),
+      div(
+        // Important: Setting the width relative to the viewport width in order to hide overflowed contents
+        style ->
+          s"overflow-x: scroll; overflow-y: scroll; max-width: calc(100vw - 184px); height: ${consoleHeight}px; max-height: ${consoleHeight}px;",
+        currentQuery
+          .lastQueryResult
+          .map { result =>
+            pre(cls -> "font-mono", printQueryResult(result))
+          }
+      )
     )
-  )
 
   def printQueryResult(result: QueryResult): String = TablePrinter.printTableRows(result)
 
