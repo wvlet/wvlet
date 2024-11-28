96d900a610963f0e06756b74510a2d4bd20b4f90
Matts966 <Matts966@users.noreply.github.com>
2024-11-29 01:06:45 +0900

    Add comment docs

--- sdks/python/wvlet/runner.py
+++ sdks/python/wvlet/runner.py
@@ -14,7 +14,6 @@
 # limitations under the License.
 #
 
-
 import sys
 from typing import Optional
 import shutil
@@ -22,7 +21,23 @@ import subprocess
 
 
 class WvletCompiler():
+    """WvletCompiler is for compiling Wvlet queries into SQL queries.
+
+    This class assumes that wvlet is installed and in PATH or that executable_path is specified.
+    """
+
     def __init__(self, executable_path: Optional[str] = None, target: Optional[str] = None):
+        """
+        Initializes the WvletCompiler with the specified executable path and target.
+
+        Args:
+            executable_path (Optional[str]): The path to the wvlet executable. If not provided, it will look for 'wvlet' in the system PATH.
+            target (Optional[str]): The target database for the compiled SQL queries.
+        
+        Raises:
+            ValueError: If the provided executable_path is invalid.
+            NotImplementedError: If the wvlet executable is not found in the system PATH.
+        """
         if executable_path:
             if shutil.which(executable_path) is None:
                 raise ValueError(f"Invalid executable_path: {executable_path}")
@@ -31,12 +46,23 @@ class WvletCompiler():
         # To make self.path non-optional type, first declare optional path
         path = shutil.which("wvlet")
         if path is None:
-            raise NotImplementedError("This binding currently requires wvc executable")
+            raise NotImplementedError("This binding currently requires wvlet executable")
         self.path = path
         self.target = target
 
-
     def compile(self, query: str) -> str:
+        """
+        Compiles the given Wvlet query into an SQL query.
+
+        Args:
+            query (str): The Wvlet query to be compiled.
+
+        Returns:
+            str: The compiled SQL query.
+
+        Raises:
+            ValueError: If the compilation process fails.
+        """
         command = [self.path, "compile"]
         if self.target:
             command.append(f"--target:{self.target}")
