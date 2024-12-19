36cd7716ea6c1b0e712b102d2198a94faeb3a274
Taro L. Saito <leo@xerial.org>
2024-12-19 13:22:44 -0800

    fix

--- wvlet-lang/src/main/scala/wvlet/lang/compiler/analyzer/TypeResolver.scala
+++ wvlet-lang/src/main/scala/wvlet/lang/compiler/analyzer/TypeResolver.scala
@@ -848,7 +848,7 @@ object TypeResolver extends Phase("type-resolver") with ContextLogSupport:
     override def apply(context: Context): PlanRewriter = { case q: Query =>
       init(context)
       q.transformUp { case s: GeneralSelection =>
-        s.transformChildExpressions(resolveAggregationExpr(using context))
+        s.transformUpExpressions(resolveAggregationExpr(using context))
       }
     }
 
