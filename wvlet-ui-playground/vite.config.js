c7d95ec2534cfecb42f06fb162386aacd9a9da4f
Taro L. Saito <leo@xerial.org>
2025-01-31 16:38:29 -0800

    ui (internal): Use vite-plugin-monaco-editor for awaiting dynamic load (#815)

--- wvlet-ui-playground/vite.config.js
+++ wvlet-ui-playground/vite.config.js
@@ -14,6 +14,7 @@
 
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import monacoEditorPlugin from "vite-plugin-monaco-editor";
 import fs from 'fs'
 import replace from '@rollup/plugin-replace';
 
@@ -32,6 +33,7 @@ export default defineConfig({
   },
   plugins: [
     tailwindcss(),
+    monacoEditorPlugin, // plugin for awaiting Monaco Editor loading
     replace({
       preventAssignment: true,
       __target__: scalaJsTarget
