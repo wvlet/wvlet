3014d1dfe1130f16612ea42f886557057fdc6f3d
Taro L. Saito <leo@xerial.org>
2025-09-08 13:40:59 -0700

    style: Apply final code formatting
    Auto-formatted SqlSpec.scala for consistent code style before merge.

--- wvlet-runner/src/test/scala/wvlet/lang/runner/SqlSpec.scala
+++ wvlet-runner/src/test/scala/wvlet/lang/runner/SqlSpec.scala
@@ -1,11 +1,12 @@
 package wvlet.lang.runner
 
-class SqlBasicSpec extends SpecRunner(
-  "spec/sql/basic",
-  ignoredSpec = Map(
-    "show-create-view.sql" -> "SHOW CREATE VIEW execution not yet fully supported"
-  )
-)
+class SqlBasicSpec
+    extends SpecRunner(
+      "spec/sql/basic",
+      ignoredSpec = Map(
+        "show-create-view.sql" -> "SHOW CREATE VIEW execution not yet fully supported"
+      )
+    )
 
 class SqlTPCHSpec extends SpecRunner("spec/sql/tpc-h", parseOnly = true, prepareTPCH = true)
 
