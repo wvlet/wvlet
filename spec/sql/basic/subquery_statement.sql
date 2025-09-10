205c26b8c169e692a98c76383bef16b106e09cc7
Taro L. Saito <leo@xerial.org>
2025-09-09 20:12:35 -0700

    fix: Replace Trino-specific functions with standard SQL in test
    Use COUNT(*) instead of array_join to ensure compatibility with DuckDB

--- spec/sql/basic/subquery_statement.sql
+++ spec/sql/basic/subquery_statement.sql
@@ -1,6 +1,6 @@
 -- Test subquery as standalone statement with VALUES
 (
-   SELECT array_join(array_agg(concat(concat('MAP(ARRAY[''tag_discriminator''], ARRAY[''', col1), '''])')), ',') f_32cde
+   SELECT COUNT(*) as count_result
    FROM
      (VALUES ('value1', 'purchase_shop_todaysdish'), ('value2', 'purchase_shop_todaysdish')) AS t(col1, col2)
    WHERE (col2 IN ('purchase_shop_todaysdish'))
